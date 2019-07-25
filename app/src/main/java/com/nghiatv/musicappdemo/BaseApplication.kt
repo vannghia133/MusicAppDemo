a89399d9c021f7fe4515f8fe3a63f82d8a0fa2fa
vannghia133 <vannghia22471001@gmail.com>
2019-07-25 08:37:08 +0700

    Initial commit

---
+++ app/src/main/java/com/nghiatv/musicappdemo/BaseApplication.kt
@@ -0,0 +1,11 @@
+package com.nghiatv.musicappdemo
+
+import android.app.Application
+
+class BaseApplication : Application() {
+    private val TAG = "BaseApplication"
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
